e15fde9d2203733463e160d70fcde29fc0823a14
Ibrahim Santana Velazquez <315158477+ibrahimsantana@users.noreply.github.com>
2026-09-09 22:09:41 -0600

    Map exact approved Quiniela sticker assets for Week 1 Hub

---
+++ js/quiniela-stickers-w01.js
@@ -0,0 +1,23 @@
+window.PMX_QUINIELA_STICKERS = Object.freeze({
+  "EL DOC":"https://media.canva.com/v2/image-resize/format:PNG/height:200/quality:100/uri:ifs%3A%2F%2FM%2F856f38c3-b476-4876-9686-5dfe4828c132/watermark:F/width:200?csig=AAAAAAAAAAAAAAAAAAAAAPRgXRcZsbt2_dA_4ICjmtV-re72JCCgrNV5mQPppn4J&exp=1789030605&osig=AAAAAAAAAAAAAAAAAAAAAIsUjeREgwHfKRRFrD73A_bsW1XBZdZKswolSkB-H6SQ&signer=media-rpc&x-canva-quality=thumbnail",
+  "ARI":"https://media.canva.com/v2/image-resize/format:PNG/height:200/quality:100/uri:ifs%3A%2F%2FM%2F20e96dc1-ef71-40d5-9222-a5b57a3f74ae/watermark:F/width:200?csig=AAAAAAAAAAAAAAAAAAAAAHQjh_wgZhi9bCcTxO_VWypg3uE7_Rp8gaL6g6DZO1Ir&exp=1789028234&osig=AAAAAAAAAAAAAAAAAAAAAM8dWWtLGc-uwgJ2Fba9NWUrfVOXETE6I5ErJN0-GGk4&signer=media-rpc&x-canva-quality=thumbnail",
+  "LALO":"https://media.canva.com/v2/image-resize/format:PNG/height:200/quality:100/uri:ifs%3A%2F%2FM%2Fd903e1ad-d444-4e12-8740-6629d582407f/watermark:F/width:200?csig=AAAAAAAAAAAAAAAAAAAAAPrAvr1reD4KNMb1WbFQkl3F2XwjCEnY6KMnEsXYB1aV&exp=1789027598&osig=AAAAAAAAAAAAAAAAAAAAAESmlMVzGDpNg-TVat2FPLZWOsV9lgY9curr-JHPPJe2&signer=media-rpc&x-canva-quality=thumbnail",
+  "ALEX":"https://media.canva.com/v2/image-resize/format:PNG/height:200/quality:100/uri:ifs%3A%2F%2FM%2F7d61312a-3507-48f0-b09c-b459ab39a4b7/watermark:F/width:200?csig=AAAAAAAAAAAAAAAAAAAAAKbh6nQe1DuwfPI8XNne3TbxV5bRP5R1nTG_2QmEFNW-&exp=1789029256&osig=AAAAAAAAAAAAAAAAAAAAAGUeLIx8uRwbcnv_62Ifw6YA-pKE1mJjfaMAYaz07MzU&signer=media-rpc&x-canva-quality=thumbnail",
+  "LUIS C.":"https://media.canva.com/v2/image-resize/format:PNG/height:200/quality:100/uri:ifs%3A%2F%2FM%2F67becc46-77fd-4c9e-93c9-74a97e539cbe/watermark:F/width:200?csig=AAAAAAAAAAAAAAAAAAAAAE_AC5hky-w_f5wdz_xIYW1qkJaKjMFIMmoZUmHlYsje&exp=1789030738&osig=AAAAAAAAAAAAAAAAAAAAALFqfubGsGNIgZPIFMdH72_MX2StXRMaCv2R9fRsDMl_&signer=media-rpc&x-canva-quality=thumbnail",
+  "DR PALMA":"https://media.canva.com/v2/image-resize/format:PNG/height:200/quality:100/uri:ifs%3A%2F%2FM%2F4be9a2e4-1db2-4a3c-bba6-9bbf3d1b7909/watermark:F/width:200?csig=AAAAAAAAAAAAAAAAAAAAAM0w6tLQrDrukGe5nsaEdky4ulMdmfYh-U3xNKmUX3vo&exp=1789028113&osig=AAAAAAAAAAAAAAAAAAAAAD-6HrjIwG-GvlRplhDc0SzAYJyyZ96UY2nbrDZ6dgLY&signer=media-rpc&x-canva-quality=thumbnail",
+  "JENNI":"https://media.canva.com/v2/image-resize/format:PNG/height:200/quality:100/uri:ifs%3A%2F%2FM%2Fec3bae90-f0fc-4721-b423-f8352de643c8/watermark:F/width:200?csig=AAAAAAAAAAAAAAAAAAAAAC3o_I0YDpZAU0S_n2U8SS3J-p2_KUeQcWClzpPsESNA&exp=1789030692&osig=AAAAAAAAAAAAAAAAAAAAAELSBv9jUeyAKyoA0gsxltwmKIia3xFyJSpR1Zcb1qkM&signer=media-rpc&x-canva-quality=thumbnail",
+  "RODRI":"https://media.canva.com/v2/image-resize/format:PNG/height:200/quality:100/uri:ifs%3A%2F%2FM%2F362517ec-a2e3-4f65-8f68-e7ad517096c1/watermark:F/width:200?csig=AAAAAAAAAAAAAAAAAAAAAEW_ogSB2fZ0btcYjs2tkGuYZvX3Fm-Rt1mC5cxxFSmS&exp=1789029340&osig=AAAAAAAAAAAAAAAAAAAAAHfG3rlL11CyJnYkPmtaSd66sD2C2mLb62ImrtYKME7b&signer=media-rpc&x-canva-quality=thumbnail",
+  "IBRACHO":"https://media.canva.com/v2/image-resize/format:PNG/height:200/quality:100/uri:ifs%3A%2F%2FM%2F7963afe2-87d3-483b-9ef2-c4562a88e95f/watermark:F/width:200?csig=AAAAAAAAAAAAAAAAAAAAAG35NgZj0d1JQTdWaj0iltGsQgED0QXwo60DmmY0Gg9z&exp=1789029018&osig=AAAAAAAAAAAAAAAAAAAAAHR_rRjWtzgyIvELptmNljv6Zh4bOibheHSw5XWvLZ7g&signer=media-rpc&x-canva-quality=thumbnail"
+});
+
+window.PMX_QUINIELA_STICKER_ASSET_IDS = Object.freeze({
+  "EL DOC":"MAHUrDGtX9Y",
+  "ARI":"MAHUrKwXexY",
+  "LALO":"MAHUrELbzLU",
+  "ALEX":"MAHUrOaW9dc",
+  "LUIS C.":"MAHUrHbU2Qg",
+  "DR PALMA":"MAHUrKIoNvw",
+  "JENNI":"MAHUrGE5fVE",
+  "RODRI":"MAHUrC-vXKE",
+  "IBRACHO":"MAHUrBPgToA"
+});
