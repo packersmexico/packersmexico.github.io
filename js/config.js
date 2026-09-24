10b0bba62895643d7c05508554748447d614ed3d
Ibrahim Santana Velazquez <315158477+ibrahimsantana@users.noreply.github.com>
2026-09-24 14:37:12 -0600

    Bump analytics schema for permanent share routes

--- js/config.js
+++ js/config.js
@@ -101,7 +101,7 @@
     analytics: Object.freeze({
       GA4_MEASUREMENT_ID: "G-QEN5F5YY14",
       hubVersion: "p0-weekly-auto-v1.2",
-      eventVersion: "1.1"
+      eventVersion: "1.2"
     }),
     socialUrls: Object.freeze({
       facebook: "https://www.facebook.com/share/1J1GHihRNz/",
